87020cbdf5c569d79ea52c895b65e23b6faaf0ce
BlaerZhang <zhangyueyang0022@126.com>
2023-04-21 04:20:48 -0400

    Change Pixelate method from texture+Canvas to shader; Added camera movement and target detacher

---
+++ Project WoHuCangLong/Assets/Scripts/TargetGroupDetacher.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class TargetGroupDetacher : MonoBehaviour
+{
+
+    private CinemachineTargetGroup tG;
+    private bool isDetached = false;
+
+   void Start()
+    {
+        tG = GetComponent<CinemachineTargetGroup>();
+    }
+
+    void Update()
+    {
+        if (!GameManager.instance.isInGame && !isDetached)
+        {
+            tG.RemoveMember(tG.m_Targets[0].target);
+            tG.RemoveMember(tG.m_Targets[0].target);
+            tG.m_Targets[0].radius = 5.5f;
+            isDetached = true;
+        }
+    }
+}
